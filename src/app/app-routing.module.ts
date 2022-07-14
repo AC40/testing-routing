a9a553f3b8b3ccdd0f3eadde58b6e4a0da4d3e60
AC40 <aaron.c.richter@icloud.com>
2022-07-14 23:06:16 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerListComponent } from './player-list/player-list.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'teams', component: TeamListComponent },
+  { path: 'players', component: PlayerListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [PlayerListComponent, TeamListComponent];
